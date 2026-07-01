b2a356f5469e5440210fd386f71d21d4b20f6ea0
Allan <allanfs0108@gmail.com>
2026-07-01 11:05:12 -0300

    Criando ProductDetailScreen.tsx além de colocar produtos dentro do array que criamos no commit passado, criando constante de estilos antes da estrutura da tela de detalhes do produto

--- screens/ProductDetalScreen.tsx
+++ screens/ProductDetalScreen.tsx
@@ -9,6 +9,7 @@ import {
     TouchableOpacity,
     SafeAreaView,
     StatusBar,
+    SafeAreaViewBase,
 } from 'react-native';
 
 import { Ionicons, Feather, MaterialCommunityIcons } from "@expo/vector-icons";
@@ -18,3 +19,109 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "./HomeScreen";
 
 import { getProdutoById } from "../data/produtos";
+
+type Props = NativeStackScreenProps<RootStackParamList, 'ProductDetail'>;
+
+export default function ProductDetail({ navigation, route }: Props) {
+    const produto { productId } = route.params;
+    const [quantidade, setQuantidade] = useState(1);
+    const insets = useSafeAreaInsets();
+
+    if (!produto) {
+        return (
+            <SafeAreaView>
+
+            </SafeAreaView>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    scroll: {
+
+    },
+    ScrollContent: {
+
+    },
+    headerButton: {
+
+    },
+    headerButtonLeft: {
+
+    },
+    hedaerButtonRight: {
+
+    },
+    productImage: {
+
+    },
+    brandRow: {
+
+    },
+    brandLogo: {
+
+    },
+    brandName: {
+
+    },
+    productName: {
+
+    },
+    priceRow: {
+
+    },
+    price: {
+
+    },
+    quantifySelector: {
+
+    },
+    quantifyButtonMinus: {
+
+    },
+    quantifyButtonPlus: {
+
+    },
+    quantifyText: {
+
+    },
+    sectionTitle: {
+
+    },
+    aboutText: {
+
+    },
+    ingredientsHeader: {
+
+    },
+    ingredientRow: {
+
+    },
+    bullet: {
+
+    },
+    ingredientText: {
+
+    },
+    bottomSpacer: {
+
+    },
+    footer: {
+
+    },
+    addButton: {
+
+    },
+    addButtonText: {
+
+    },
+    errorText: {
+
+    },
+    backLink: {
+
+    },
+})
